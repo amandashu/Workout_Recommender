eb1a7f5d832a8b926a7166572ca7ee5984ba6d38
Peter Peng <peterpeng@Madelyn.san.rr.com>
2020-12-04 15:46:15 -0800

    Added scraper for all links

--- src/data/scrape.py
+++ src/data/scrape.py
@@ -1,12 +1,45 @@
 import os
 import csv
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 def get_workout_links():
     """
     Goes through all (free?) workouts and returns lists of individual workout links
     """
-    return ['https://www.fitnessblender.com/videos/daniel-s-upper-body-strength-workout-for-people-who-get-bored-easily']
+    driver = webdriver.Chrome(os.path.abspath('../chromedriver'))
+    driver.get("https://www.fitnessblender.com/videos?exclusive%5B%5D=0")
 
+    all_links = []
+    for page in range(29):
+        print(page)
+
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "contents"))
+        )
+
+        time.sleep(2)
+        links = [content.get_attribute('href') for content in driver.find_elements_by_class_name('contents') if content.get_attribute('href') != None]
+        all_links.append(links)
+
+        next_btn = driver.find_element_by_class_name('iconfont-arrow-forward')
+        next_btn.click()
+    driver.close()
+
+    with open('all_links.pickle', 'wb') as f:
+        pickle.dump([i for j in all_links for i in j if i is not None], f)
+
+    return [i for j in all_links for i in j if i is not None]
+
+
+if __name__ == "__main__":
+    get_workout_links()
+    
 
 def get_data(link):
     """
