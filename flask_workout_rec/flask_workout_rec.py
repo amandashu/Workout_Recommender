880afee1f309b3212a80f76ee756561ba8952778
amandashu <shu.amanda25@gmail.com>
2021-01-12 13:08:12 -0800

    add basic flask app

---
+++ flask_workout_rec/flask_workout_rec.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# dummary_data
+data = [
+    {
+        'name': "Daniel's Upper Body...",
+        #'calorie':
+        #'duration': 39
+        #'difficulty':,
+        #'equipment':
+        #'training type'
+    },
+    {
+        'name': "HIIT Pilates Strength...",
+    }
+
+]
+
+
+
+@app.route('/')
+def recommendation_page():
+    return render_template('recommendation_page.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
