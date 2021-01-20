1e025fa2e70315f499e6ff575e4f8405fe3afd27
amandashu <shu.amanda25@gmail.com>
2021-01-20 12:13:34 -0800

    move base html to recommendation page, navigation bar links, testing connection to mysql database

--- flask_website/flask_workout_rec.py
+++ flask_website/flask_workout_rec.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template, redirect, url_for
 from forms import RegistrationForm, LoginForm
+from flask_mysqldb import MySQL
+import json
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'dev'
+
+db_config = json.load(open('./db_config.json'))
+
+app.config['MYSQL_HOST'] = db_config['mysql_host']
+app.config['MYSQL_USER'] = db_config['mysql_user']
+app.config['MYSQL_PASSWORD'] = db_config['mysql_password']
+app.config['MYSQL_DB'] = db_config['mysql_db']
+
+mysql = MySQL(app)
 
 # dummy_data
 data = [
@@ -19,7 +31,6 @@ data = [
     }
 
 ]
-
 
 @app.route('/register', methods=['GET', 'POST'])
 def registration_page():
@@ -38,6 +49,17 @@ def login_page():
 @app.route('/')
 def recommendation_page():
     return render_template('recommendation_page.html', data=data)
+
+@app.route('/about')
+def about_page():
+    return render_template('about_page.html')
+
+@app.route('/contact')
+def contact_page():
+    cur = mysql.connection.cursor()
+    query = cur.execute("SELECT id FROM testing")
+    results = cur.fetchall()
+    return render_template('contact_page.html', query = results)
 
 if __name__ == '__main__':
     app.run(debug=True)
