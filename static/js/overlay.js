b5ad5adbff45a5a79636675a68ba68864bc2e5ed
Najeem <contact@najeemkanishka.com>
2021-02-12 11:40:19 -0800

    like button reads workout id now

--- static/js/overlay.js
+++ static/js/overlay.js
@@ -14,14 +14,32 @@ function openPlayerOverlay(videoIndex) {
     videoTitle = document.getElementById("workout-title-index-" + videoIndex).textContent
     videoDesc = document.getElementById("workout-text-index-" + videoIndex).innerText
     fbURL = document.getElementById("fb-link-index-" + videoIndex).innerText
+    workout_id = document.getElementById("startWorkout-index-" + videoIndex).getAttribute("workout_id");
+    original_url = document.getElementById('like').href;
+    
 
     document.getElementById("overlay-video").src = ytURL;
     document.getElementById("overlay-fb-link").href = fbURL;
     document.getElementById("overlay-workout-title").textContent = videoTitle
     document.getElementById("overlay-workout-text").innerText = videoDesc
+    document.getElementById('like').setAttribute("href", original_url.substr(0, original_url.lastIndexOf("/") + 1) + workout_id);
+    
 
     document.getElementById("overlay-display").style.display = "block";
+    console.log(workout_id)
 
+
+    /*
+    document.getElementById("startWorkout-index" + videoIndex).addEventListener("click", function () {
+        sessionStorage.setItem('workout_id') = document.getElementById("startWorkout").getAttribute("workout_id");
+
+        var original_url = document.getElementById('like').href;
+
+        document.getElementById('like').setAttribute("href", original_url.substr(0, original_url.lastIndexOf("/")) + sessionStorage.getItem('workout_id'));
+
+        console.log(sessionStorage.getItem('workout_id'));
+      });
+    */
 }
 
 
