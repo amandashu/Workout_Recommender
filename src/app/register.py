9e581583b3d9b1adfe728ef7d91229ae4821a243
Najeem Kanishka <contact@najeemkanishka.com>
2021-02-14 19:21:35 -0800

    Merged dev_AS_flask into dev
    * update preferences that prepopulates with user's preferences
    
    * implement updating preferences
    
    * fixed telescoping body focus issue
    
    Co-authored-by: amandashu <shu.amanda25@gmail.com>

--- src/app/register.py
+++ src/app/register.py
@@ -1,7 +1,10 @@
-def register_user(form, user_id, hashed_password):
+def get_equipment_training_types(form):
     """
-    Takes wtforms object, user_id, hashed_password, and returns arguments
-    to execute query (query string, tuple of variables)
+    Takes in wtfforms and returns:
+    - list of equipment and training types (column names)
+    - string of user's inputted available equipment
+    - string of user's inputted preferred training types
+    - tuple of equipment/training values (1 or 0 if user clicked those options)
     """
     # dictionary with equipment_training_types as values and 0 or 1 for user's preference
     equipment_training_types = ['balance_agility', 'barre', 'cardiovascular', 'hiit',
@@ -36,6 +39,21 @@ def register_user(form, user_id, hashed_password):
         for x in form.training_type.choices:
             dct[x[0]] = 1
 
+    # tuples of values to insert
+    equipment_training_types_tup = tuple([dct[v] for v in dct.keys()])
+
+    return equipment_training_types, equipment_string, training_type_string, \
+        equipment_training_types_tup
+
+def register_user(form, user_id, hashed_password):
+    """
+    Takes wtforms object, user_id, hashed_password, and returns arguments
+    to execute query (query string, tuple of variables) for insertion of a
+    new user into users table
+    """
+    equipment_training_types, equipment_string, training_type_string, \
+            equipment_training_types_tup = get_equipment_training_types(form)
+
     # string of column names
     cols = ['user_id', 'name', 'email', 'password', 'equipment',
             'training_type', 'min_duration', 'max_duration', 'min_calories',
@@ -49,13 +67,35 @@ def register_user(form, user_id, hashed_password):
     # insert string
     insert  = 'INSERT INTO users(' + cols_string +') VALUES(' + values + ')'
 
-    # tuples of values to insert
-    equipment_training_types_tup = tuple([dct[v] for v in dct.keys()])
-
-    # print(len(equipment_training_types_tup))
+    # tuple of values to insert
     tup = (user_id, form.name.data, form.email.data, hashed_password,
              equipment_string, training_type_string, form.min_duration.data,
              form.max_duration.data, form.min_calories.data,
              form.max_calories.data, form.min_difficulty.data,
               form.max_difficulty.data, *equipment_training_types_tup)
     return insert, tup
+
+def update_preferences(form, user_id):
+    """
+    Takes wtform object, user_id, and returns arguments to execute query (query
+    string, tuple of variables) for updating a user's information in user table
+    """
+    # inputted equipment/training_types
+    equipment_training_types, equipment_string, training_type_string, \
+            equipment_training_types_tup = get_equipment_training_types(form)
+
+    # string of column names
+    cols = ['equipment','training_type', 'min_duration', 'max_duration', 'min_calories',
+            'max_calories', 'min_difficulty', 'max_difficulty']
+    cols += equipment_training_types
+    cols_string = '=%s, '.join(cols) + '=%s'
+
+    # string for update statement
+    update_string = 'UPDATE users SET ' + cols_string + " WHERE user_id= " + str(user_id)
+
+    # tuple of values to insert
+    tup = (equipment_string, training_type_string, form.min_duration.data,
+             form.max_duration.data, form.min_calories.data,
+             form.max_calories.data, form.min_difficulty.data,
+              form.max_difficulty.data, *equipment_training_types_tup)
+    return update_string, tup
